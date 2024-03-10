fb2369cf4e40fdb767beea5ff597756ac57e69f4
unknown <voldam@ lolamgit config --global user.email volidam>
2024-03-10 16:22:38 +0300

    it was my homework

---
+++ main.js
@@ -0,0 +1,22 @@
+let numberFirst = Number(prompt("enter first number = "));
+let numberSecond = Number(prompt("enter second number = "));
+
+let sum, a , b ,c;
+
+
+sum = numberFirst + numberSecond;
+a = numberFirst / numberSecond;
+b = numberFirst - numberSecond;
+c = Math.abs(numberFirst,numberSecond);
+
+
+console.log(sum);
+console.log(a);
+console.log(b);
+console.log(c);
+
+// let number = 5;
+// let ruslt, result2;
+// ruslt = Math.pow(number, 9);
+// result2 = Math.sqrt(9)
+// alert(ruslt + "/n" + result2);
